bc146cf54226fcd1caf74e2c1e4a26f53f76f487
Arthur Passos <54548671+Tutuviz@users.noreply.github.com>
2019-09-28 23:05:02 -0300

    Update Impar ou par.c

--- Impar ou par.c
+++ Impar ou par.c
@@ -8,7 +8,11 @@ main () {
     scanf ("%d",&N2);
 
     if ((N1+N2) % 2 == 0)
+    {
         printf ("%d que eh a soma dos 2 numeros eh par",N1+N2);
+    }
     else
+    {
         printf ("%d que eh a soma dos 2 numeros eh impar",N1+N2);
+    }
 }
