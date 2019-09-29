ca4f33277d96c9a13da101b0c39e6e414e94a024
Arthur Passos <54548671+Tutuviz@users.noreply.github.com>
2019-09-28 21:33:42 -0300

    Add files via upload

---
+++ Impar ou par.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main () {
+    int N1, N2;
+    printf ("Manda ae um numero: ");
+    scanf ("%d",&N1);
+    printf ("Manda ae outro numero: ");
+    scanf ("%d",&N2);
+
+    if ((N1+N2) % 2 == 0)
+        printf ("%d que eh a soma dos 2 numeros eh par",N1+N2);
+    else
+        printf ("%d que eh a soma dos 2 numeros eh impar",N1+N2);
+}
