ea756753f1d30885612ae916e834d98a0e8138ed
Arthur Passos <54548671+Tutuviz@users.noreply.github.com>
2019-09-29 12:55:21 -0300

    Update Positivo ou Negativo.c

--- Positivo ou Negativo.c
+++ Positivo ou Negativo.c
@@ -4,13 +4,16 @@ main () {
     int N1;
     printf ("Manda ae um numero: ");
     scanf ("%d",&N1);
-    if (N1>0)
+    if (N1>0) {
         printf ("%d eh positivo man",N1);
-    else
-        if (N1<0)
+    }
+    else {
+        if (N1<0) {
             printf ("%d eh negativo man",N1);
-        else 
+        }
+        else { 
             printf ("%d eh zero man",N1);
-    
+        }
+    }
 
 }
