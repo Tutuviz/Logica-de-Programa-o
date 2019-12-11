eaae9862d0943c3f363d61c7d0c7b35bc19cd407
Arthur Passos <54548671+Tutuviz@users.noreply.github.com>
2019-12-11 16:41:48 -0300

    Update Binario.c

--- Utilidades/Binario.c
+++ Utilidades/Binario.c
@@ -1,33 +1,24 @@
 #include <stdio.h>
 
-int count = 0;
-int loop;
-
 main(){
-    int binary[count];
-    int inputValue;
+    int count = 0, loop, binary[200], inputValue, x;
     
     printf ("Digite um numero: ");
     scanf ("%d", &inputValue);
 
     for (loop = -1; loop < count; loop++){
-        if (inputValue == 1)  {
-            binary[count] = 1;
-        }
-        else
-        {
-            if (inputValue % 2 == 1 ){
-                binary[count] = 1;
-            }
-            if (inputValue % 2 == 0){
-                binary[count] = 0;
-            }
+        if (inputValue > 1){
+            x = inputValue % 2;
+            binary[count] = x; 
             count++;
-            inputValue = inputValue/2;
+            inputValue = inputValue/2; 
+        }
+        else {
+            binary[count] = inputValue;
         }
     }
     
-    for (loop = count; loop > -1; loop--){
+    for (loop = count; loop > -1; loop--) {
         printf("%d", binary[loop]);  
     }
 }
